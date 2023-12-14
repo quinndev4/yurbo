ca76087e75e7465e6301d82cd6fd00329f177e0b
Quinn Nolan <quinndevtest@gmail.com>
2023-12-13 22:52:57 -0600

    WIP

---
+++ src/app/components/SinglePageForm.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import * as Yup from 'yup';
+import { FormikConfig, FormikValues, useFormik } from 'formik';
+import { YUP } from '../constants/constants';
+
+interface Values {
+  location: string;
+}
+
+interface GenericValues<T> {
+  formikConfig: FormikConfig<T>;
+}
+let SinglePageForm: React.FC<GenericValues>;
+export default SinglePageForm = ({ formikConfig }) => {
+  const formik = useFormik<FormikValues>(formikConfig);
+
+  return (
+    <div className='2xs:w-4/5 xl:w-1/2 bg-blue-600'>
+      <form className='flex flex-col m-5' onSubmit={formik.handleSubmit}>
+        <div className='flex flex-col w-full items-start my-5'>
+          <label htmlFor='location'>Location *</label>
+          <input
+            className='text-black text-left w-1/4'
+            id='location'
+            type='text'
+            placeholder='Enter Location'
+            {...formik.getFieldProps('location')}
+          />
+          <p
+            className={`text-red-600 h-5 ${
+              !formik.errors.location && 'invisible'
+            }`}
+          >
+            {formik.errors.location}
+          </p>
+        </div>
+
+        <button className='btn-primary m-auto' type='submit'>
+          Submit
+        </button>
+      </form>
+    </div>
+  );
+};
--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,3 +1,42 @@
 declare type OkRes = {
   ok: string;
 };
+
+declare interface GenericFormFieldComponent<T> extends T {
+  id: string;
+  label: string;
+  error: string | undefined;
+}
+
+declare interface GenericFormField<T> {
+  yup: Yup.Object;
+  initialValue: T;
+  Component: React.ComponentType<GenericFormFieldComponent<T>>;
+}
+
+declare interface GenericFormProps<> {
+  fields: GenericFormField[];
+  onSubmit: FormEventHandler<HTMLFormElement>;
+}
+
+/*
+
+declare interface GenericFormFieldComponent
+  extends HTMLProps<HTMLInputElement> {
+  id: string;
+  label: string;
+  error: string | undefined;
+}
+
+declare interface GenericFormField<T> {
+  yup: Yup.Object;
+  Component: React.ComponentType<GenericFormFieldComponent>;
+}
+
+declare interface GenericFormProps<> {
+  fields: GenericFormField[];
+  onSubmit: FormEventHandler<HTMLFormElement>;
+}
+
+
+*/

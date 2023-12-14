840468f46db4a5481da33066a3d33494cfc38af4
Quinn Nolan <quinndevtest@gmail.com>
2023-12-13 20:28:54 -0600

    add base formik form

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
+import Link from 'next/link';
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
-      Dave
+      <Link href={'/submit'} className='rounded-lg border-2 border-white'>
+        Create a new Yurbo
+      </Link>
     </main>
   );
 }
---
+++ src/app/submit/page.tsx
@@ -0,0 +1,11 @@
+import Form from './Form';
+
+export default function SubmitForm() {
+  return (
+    <main className='flex flex-col text-center items-center'>
+      <h1>Submit A New Yurbo</h1>
+
+      <Form />
+    </main>
+  );
+}

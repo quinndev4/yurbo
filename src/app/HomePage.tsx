126210809620bdebc3f3526ddf583bd38ee33b0f
Quinn Nolan <quinndevtest@gmail.com>
2023-12-13 21:40:49 -0600

    add loading spinner when waiting on auth

--- src/app/HomePage.tsx
+++ src/app/HomePage.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <div>
+    <div className='flex flex-col'>
       <Link href={'/yurbo/create'} className='rounded-lg border-2 border-white'>
         Create a new Yurbo
       </Link>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useSession, signIn } from 'next-auth/react';
 import HomePage from './HomePage';
+import { RotatingLines } from 'react-loader-spinner';
 
 export default function Home() {
   const { data: session, status } = useSession();
@@ -10,6 +11,16 @@ export default function Home() {
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       {session && status === 'authenticated' ? (
         <HomePage />
+      ) : status === 'loading' ? (
+        <div className='m-auto'>
+          <RotatingLines
+            strokeColor='grey'
+            strokeWidth='5'
+            animationDuration='0.75'
+            width='96'
+            visible={true}
+          />
+        </div>
       ) : (
         <div className='flex flex-col'>
           <h1 className='text-3xl font-bold'>Yurbo</h1>

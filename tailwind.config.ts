f7c440e110cf4546610530a223c35322177297a9
Quinn Nolan <quinndevtest@gmail.com>
2023-12-13 20:28:22 -0600

    config screens

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -13,8 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      screens: { '3xs': '256px', '2xs': '384px', xs: '512px', '3xl': '1792px' },
     },
   },
   plugins: [],
-}
-export default config
+};
+export default config;

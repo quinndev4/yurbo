e1b23bb41f5f9d071f7f59ad642417651cebc152
quinndev4 <quinndevtest@gmail.com>
2025-06-03 20:36:56 -0500

    nothing much

--- src/app/(user)/[userId]/yurbos/page.tsx
+++ src/app/(user)/[userId]/yurbos/page.tsx
@@ -1,10 +1,12 @@
 'use client';
 
 import MapWithSidebar from '@/components/SideBarMap';
-import { useSelectedUser } from '@/providers/SelectedUserProvider';
+// import { manyYurbos } from '@/mocks/manyYurbos';
+// import { useSelectedUser } from '@/providers/SelectedUserProvider';
+import { manyYurbos as yurbos } from '@/mocks/manyYurbos';
 
 export default function UserYurbosPage() {
-  const { yurbos } = useSelectedUser();
+  // const { yurbos } = useSelectedUser();
 
   return <MapWithSidebar yurbos={yurbos} />;
 }
